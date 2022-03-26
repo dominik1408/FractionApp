af9730a88ab1a0a4d4c9b164e5bcc408a863805b
dominik.kasprzak <dominik.kasprzak@PK1-12>
2022-03-26 14:46:46 +0100

    Dodaj pliki projektów.

---
+++ FractionApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FractionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
